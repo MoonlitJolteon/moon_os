74d871afe34f5cd54c6f87fbf6b4bde9aaa683a0
Moonlit Jolteon <moonlit@munebase.dev>
2025-02-10 11:17:26 -0500

    Initial Memory mapping added

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,10 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 pub fn init() {
     gdt::init();
@@ -71,10 +75,13 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
-/// Entry point for `cargo xtest`
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+/// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
